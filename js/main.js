ee8e695603fd3c3a10c99e0c421993ff4c2fd3b5
Jorge Jesus <johndoe@example.com>
2023-08-11 09:40:59 +0200

    player in the middle

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 class Player {
     constructor() {
-        this.positionX = 50;
-        this.positionY = 0;
         this.width = 20;
         this.height = 10;
+        this.positionX = 50 - (this.width / 2);
+        this.positionY = 0;
         this.domElement = null;
         
         this.createDomElement();
@@ -90,8 +90,20 @@ setInterval(() => {
 setInterval(() => {
     obstaclesArr.forEach((obstacleInstance) => {
         obstacleInstance.moveDown();
+
+        if (
+            player.positionX < obstacleInstance.positionX + obstacleInstance.width &&
+            player.positionX + player.width > obstacleInstance.positionX &&
+            player.positionY < obstacleInstance.positionY + obstacleInstance.height &&
+            player.positionY + player.height > obstacleInstance.positionY
+          ) {
+            // Collision detected!
+            console.log("game over my fren! ");
+            location.href = "./gameover.html";
+          } 
+
     });
-}, 100);
+}, 100); 
 
 
 
